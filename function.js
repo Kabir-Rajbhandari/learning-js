9928a7f218e8eb36d2261342df5447215a670cb3
Kabir-Rajbhandari <rajbhandarikabir814@gmail.com>
2025-07-08 18:21:17 +0545

    some changes

---
+++ function.js
@@ -0,0 +1,16 @@
+// learning function
+
+// return function
+function addNumber(x, y) {
+  // (x,y) function parameters
+  let sumValue = x + y;
+  return sumValue;
+}
+
+console.log(addNumber(45, 50)); // (values) function arguments
+
+function greet() {
+  console.log("Namaste ");
+}
+
+greet();
---
+++ return.js
@@ -0,0 +1,6 @@
+function sum(x, y) {
+  let addValue = x + y;
+  return addValue;
+}
+
+console.log(sum(1, 2));
