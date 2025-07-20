cfeb355582533a8b2b39c80082abb21ebc0bae43
Kabir-Rajbhandari <rajbhandarikabir814@gmail.com>
2025-07-20 20:12:14 +0545

    function in js

--- array.js
+++ array.js
@@ -32,7 +32,7 @@ console.log(nestArr[5][0]); // abcd
 
 // array method
 
-let numbers = [1, 2, 3, 4, 5];
+let numbers = [78, 94, 12, 23, 58, 10, 89];
 let letters = ["p", "q", "r", "s", "t", "u"];
 
 // concat () method
@@ -59,4 +59,99 @@ console.log(fruits);
 fruits.unshift("blueberries");
 console.log(fruits);
 
-//
+// includes() method ---> return boolean values in output
+console.log(fruits.includes("strawberry")); // false
+console.log(fruits.includes("orange")); // true
+
+// join () method
+
+console.log(fruits.join("-"));
+
+// reverse()
+console.log(fruits.reverse());
+
+// slice () method
+
+console.log(fruits.slice(0, 3)); // provide the value of index 0 to 2;
+
+//sort() method
+console.log(numbers.sort());
+
+// objects [collectionn of data stores in a key:value pair]
+
+let person = { name: "abcdef", age: 18, email: "abcd1234@gmail.com" };
+console.log(typeof person);
+console.log(person);
+
+// accessing items from objects
+console.log(person.email); // 1st way
+console.log(person["age"]); // 2nd way
+
+person.id = "a1"; // added the id = a1 in  the person objects
+console.log(person);
+
+//delete from objects
+
+delete person.age;
+
+// simple exercise
+
+function carDetails() {
+  let car = { type: "BMW", model: "2025", color: "black" };
+  console.log(typeof car);
+
+  console.log(car["type"]);
+
+  car.type = "Toyota";
+  console.log(car);
+
+  car.wheels = 4;
+  console.log(car);
+}
+carDetails();
+
+// function with parameters
+
+function userDetails(firstname, lastName) {
+  let detailsOne = firstname;
+  let detailsTwo = lastName;
+  let combine = firstname + " " + lastName;
+
+  console.log(`Hello ${combine}`);
+}
+
+userDetails("abcd", "pqrs");
+
+// return in functions
+
+function numAdd(x, y) {
+  return x + y;
+}
+
+console.log(numAdd(45, 50));
+
+// simple exercise done on function
+function myFunction(num1, num2) {
+  let value1 = num1;
+  let value2 = num2;
+  let multiply = value1 * value2;
+  console.log(`The multiplication of ${value1} and ${value2} is ${multiply}.`);
+}
+
+myFunction(5, 15);
+
+// function declearation
+
+function functDeclear(userName) {
+  console.log(`Hello ${userName}`);
+}
+
+functDeclear("World");
+
+// function expression
+
+const functVariable = function functExpress(number) {
+  return number;
+};
+
+console.log(functVariable(78));
